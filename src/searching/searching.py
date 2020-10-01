7fc9f07939da3ea6dfd253721f7c20edeae00c3d
Dominique Garrett <dmnqgrrtt@gmail.com>
2020-09-30 23:07:55 -0400

    searches are passing

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,8 @@
 def linear_search(arr, target):
     # Your code here
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
 
     return -1   # not found
@@ -9,6 +12,22 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    first = 0
+    last = (len(arr) - 1)
+
+    found = False
+
+    while not found and len(arr) > 0:
+        middle = (first + last) // 2
+
+        if arr[middle] == target:
+            found = True
+            return middle
+        else:
+            if target < arr[middle]:
+                last = middle - 1
+            else:
+                first = middle + 1
 
 
     return -1  # not found
